2d7e4600abc31ac2934bd0eaab11a48b9b03f3db
ahmed <ahmedmohmed12363@yahoo.com>
2025-03-01 11:29:42 +0200

    ci(config files): implement logger add - winston config file add - packages

--- apps/server/src/main.js
+++ apps/server/src/main.js
@@ -1,10 +1,24 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 import express from 'express';
+import connectToDB from './database/connect';
+import logger from './lib/winston/index.js';
+
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
+
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
-});
+
+connectToDB()
+  .then(() => {
+    app.listen(port, host, () => {
+      logger.info(`[ ready ] http://${host}:${port}`);
+    });
+  })
+  .catch((e) => {
+    logger.error(`could not connect to database ${e}`);
+  });
