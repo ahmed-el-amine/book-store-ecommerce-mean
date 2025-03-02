daf9475281345d0d5bdc4b2461d01e95a864140c
Ahmed Essam <ahmed.essam.sharak@gmail.com>
2025-03-02 10:49:22 +0200

    feat(main): add global uncaught exception and unhandled rejection handlers for improved error management

--- apps/server/src/main.js
+++ apps/server/src/main.js
@@ -5,6 +5,13 @@ import express from 'express';
 import connectToDB from './database/connect';
 import logger from './lib/winston/index.js';
 
+// Placed first to caught any uncaught exception in the program
+process.on('uncaughtException', (err) => {
+  logger.error('UNCAUGHT EXCEPTION! Shutting down...');
+  logger.error(err.name, err.message);
+  process.exit(1);
+});
+
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
@@ -13,12 +20,17 @@ app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
-connectToDB()
-  .then(() => {
-    app.listen(port, host, () => {
-      logger.info(`[ ready ] http://${host}:${port}`);
-    });
-  })
-  .catch((e) => {
-    logger.error(`could not connect to database ${e}`);
+const server = app.listen(port, host, () => {
+  logger.info(`[ ready ] http://${host}:${port}`);
+});
+
+connectToDB();
+
+// Placed at the bottom to handle any promise rejection in our app
+process.on('unhandledRejection', (err) => {
+  logger.error('UNHANDLED REJECTION! Shutting down...');
+  logger.error(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
   });
+});
