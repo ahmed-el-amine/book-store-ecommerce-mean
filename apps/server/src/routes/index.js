681b9ba12c462477e85f694c686511c01bd9c288
NadaEmamm <nadaemam.me@gmail.com>
2025-03-03 02:27:30 +0200

    feat(review-model): create review controllers and route with validation using zod

--- apps/server/src/routes/index.js
+++ apps/server/src/routes/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import authRouter from './auth.router.js';
 import userRouter from './user.router.js';
+import reviewRouter from './review.router.js';
 
 const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
+router.use('/reviews', reviewRouter);
+
 
 export default router;
