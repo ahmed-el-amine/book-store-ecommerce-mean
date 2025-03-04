1b9d3a5f19662adfd7f90fcc40dca65c129e509a
Fares Edres <faresedres1@gmail.com>
2025-03-04 16:02:52 +0200

    feat(order-logic):Implements order's logic and enhance schema

--- apps/server/src/routes/index.js
+++ apps/server/src/routes/index.js
@@ -2,14 +2,16 @@ import express from 'express';
 import authRouter from './auth.router.js';
 import userRouter from './user.router.js';
 import bookRouter from './book.router.js';
-import reviewRouter from './review.router.js'
+import reviewRouter from './review.router.js';
+import orderRouter from './order.router.js';
 
 const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
-router.use('/books',bookRouter);
+router.use('/books', bookRouter);
 router.use('/reviews', reviewRouter);
+router.use('/orders', orderRouter);
 
 
 export default router;
