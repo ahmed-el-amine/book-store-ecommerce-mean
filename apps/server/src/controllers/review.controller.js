9420d22219b5840f0bac961d907f1b7420bbeffc
ahmed <ahmedmohmed12363@yahoo.com>
2025-03-04 15:26:26 +0200

    refactor(refactor code) - add http status code package for more clean code - throw errors using AppError for more clean code - handle errors in error middleware - change some codes

--- apps/server/src/controllers/review.controller.js
+++ apps/server/src/controllers/review.controller.js
@@ -1,55 +1,42 @@
-import Review from '../database/models/reviews.model.js'
+import Review from '../database/models/reviews.model.js';
 import AppError from '../utils/customError.js';
+import httpStatus from 'http-status';
 
-
-export const getByBookId = async (id, next) => {
-    const reviews = await Review
-        .find({ bookIdid: id })
-        .exec();
-    if (!reviews) {
-        const err = new AppError('Review not found');
-        return next(err)
-    }
-    return reviews;
-
+export const getByBookId = async (id) => {
+  const reviews = await Review.find({ bookIdid: id }).exec();
+  if (!reviews) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Review not found');
+  }
+  return reviews;
 };
 
 export const create = async (data) => {
+  const review = await Review.create(data);
 
-    const review = await Review.create(data);
-
-    return review;
+  return review;
 };
 
 export const deleteReview = async (req, res, next) => {
+  const deletedReview = await Review.findById(req.params.id).exec();
 
-
-    const deletedReview = await Review.findById(req.params.id).exec();
-
-    if (!deletedReview) {
-        const err = new AppError('Review not found');
-        return next(err)
-    }
-    if (deletedReview.userId.toString() !== req.user.userId) {
-        return res.status(403).json({ message: 'You are not authorized to delete this review' });
-    }
-    await deletedReview.delete();
-    return deletedReview;
+  if (!deletedReview) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Review not found');
+  }
+  if (deletedReview.userId.toString() !== req.user.userId) {
+    return res.status(403).json({ message: 'You are not authorized to delete this review' });
+  }
+  await deletedReview.delete();
+  return deletedReview;
 };
 export const updateReview = async (req, res, next) => {
-    const review = await Review.findById(req.params.id).exec();
+  const review = await Review.findById(req.params.id).exec();
 
-    if (!review) {
-        const err = new AppError('Review not found');
-        return next(err)
-    }
-    if (review.userId.toString() !== req.user.userId) {
-        return res.status(403).json({ message: 'You are not authorized to update this review' });
-    }
-    const updatedReview = await Review.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true }
-    );
-    return updatedReview;
-}
+  if (!review) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Review not found');
+  }
+  if (review.userId.toString() !== req.user.userId) {
+    return res.status(403).json({ message: 'You are not authorized to update this review' });
+  }
+  const updatedReview = await Review.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  return updatedReview;
+};
--- apps/server/src/utils/customError.js
+++ apps/server/src/utils/customError.js
@@ -1,11 +1,13 @@
 class AppError extends Error {
-  constructor(message, statusCode) {
+  constructor(statusCode, message, isOperational = true, stack = '') {
     super(message);
-
     this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-    this.isOperational = true;
-    Error.captureStackTrace(this, this.constructor);
+    this.isOperational = isOperational;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
 
