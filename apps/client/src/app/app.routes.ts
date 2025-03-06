56f96d38e4a864a61ce835c085c6be3f2a0b54e0
Fares Edres <faresedres1@gmail.com>
2025-03-06 15:02:12 +0200

    feat(orders):Implement order history static component

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Route } from '@angular/router';
 import { BookDetailsComponent } from './book-details/book-details.component';
-import { CartComponent } from './components/cart/cart.component';
+import { CartComponent } from './components/Cart/cart.component';
 import { HomePageComponent } from './components/homePage/homePage.component';
+import { OrdersHistoryComponent } from './components/orders-history/orders-history.component';
 
 export const appRoutes: Route[] = [
 
@@ -16,6 +17,11 @@ export const appRoutes: Route[] = [
       path:'',
       component:HomePageComponent,
       title:'Home'
+    },
+    {
+      path:'orders-history',
+      component:OrdersHistoryComponent,
+      title:'Order History'
     }
 
 
