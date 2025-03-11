47cebb4f9ea4edcc59fb23825343cea3fa7b1726
Mustafa Ashraf <mustafaashraf123@yahoo.com>
2025-03-11 02:55:26 +0200

    docs:last changes before merge

--- apps/server/src/controllers/book.controller.js
+++ apps/server/src/controllers/book.controller.js
@@ -9,7 +9,8 @@ const getBooks = async (req) => {
     filter.categories = { $in: [categories] };
   }
   if (title) {
-    filter.title = { $regex: title, $options: 'i' };  }
+    filter.title = { $regex: title, $options: 'i' };
+  }
   if (price) {
     filter.price = { $gte: price };
   }
@@ -17,18 +18,16 @@ const getBooks = async (req) => {
   if (rating) {
     filter.rating = { $gte: rating };
   }
-  console.log('Filter:', filter); 
-  const books = await BookModel.find(filter).populate('authors').exec();
-  return books;
+  console.log('Filter:', filter);
+  const books = await BookModel.findEssentials(filter).populate('authors').exec();
+  return books.map((book) => book.toEssentials());
 };
-
 
 const getBook = async (id) => {
   const book = await BookModel.findById(id).populate('authors').exec();
   if (!book) throw new AppError(404, 'Book not found try again');
-  return book;
-}
-
+  return book.toDetails();
+};
 
 const addBook = async (data) => {
   const { title, isbn13, description, price, rating, publish_date, stock, coverImage, dimensions, weight, authors, categories } = data;
@@ -36,38 +35,31 @@ const addBook = async (data) => {
     title,
     isbn13,
     description,
-    price, rating,
+    price,
+    rating,
     publish_date,
     stock,
     coverImage,
     dimensions,
     weight,
     authors,
-    categories
+    categories,
   });
   return book;
-}
+};
 
 const updateBook = async (data, id) => {
   const book = await BookModel.findById(id).exec();
   if (!book) throw new AppError(404, `Book with ID ${id} not found please try again!`);
   const updatedBook = BookModel.findByIdAndUpdate(id, data, { runValidators: true });
   return updatedBook;
-}
+};
 
 const deleteBook = async (id) => {
   const book = await BookModel.findById(id).exec();
   if (!book) throw new AppError(404, `Book with ID ${id} not found please try again!`);
   const deletedBook = BookModel.findByIdAndDelete(id).exec();
   return deletedBook;
-}
+};
 
-
-
-export {
-  getBooks,
-  getBook,
-  addBook,
-  updateBook,
-  deleteBook
-}
+export { getBooks, getBook, addBook, updateBook, deleteBook };
--- apps/server/src/database/models/book.model.js
+++ apps/server/src/database/models/book.model.js
@@ -1,81 +1,120 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 import authorModel from '../models/author.model.js';
 import AppError from '../../utils/customError.js';
 
-const BookModel = new mongoose.Schema({
-  title: {
-    type: String,
-    minLength: [5, 'Book title must be 5 character at least'],
-    required: true,
-    unique: true,
-  },
-  isbn13: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function (v) {
-        return v.length === 13;
+const BookModel = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      minLength: [5, 'Book title must be 5 character at least'],
+      required: true,
+      unique: true,
+    },
+    isbn13: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return v.length === 13;
+        },
+        message: 'ISBN-13 must be 13 characters',
       },
-      message: 'ISBN-13 must be 13 characters'
-    }
-  },
-  description: {
-    type: String,
-    required: true,
-    minLength: [15, 'Book description must be 15 character at least']
-  },
-  price: {
-    type: Number,
-    required: true,
-    index: true
-  },
-  rating: {
-    type: Number,
-    required: true
-  },
-  publish_date: {
-    type: Date,
-    required: true,
-    index: true
-  },
-  stock: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  coverImage: {
-    type: String,
-    required: true
-  },
-  dimensions: {
-    width: Number,
-    height: Number,
-    depth: Number,
-    unit: {
+    },
+    description: {
       type: String,
-      default: 'cm'
-    }
-  },
-  weight: {
-    value: Number,
-    unit: {
+      required: true,
+      minLength: [15, 'Book description must be 15 character at least'],
+    },
+    price: {
+      type: Number,
+      required: true,
+      index: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    publish_date: {
+      type: Date,
+      required: true,
+      index: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    coverImage: {
       type: String,
-      default: 'g'
-    }
-  }
-  ,
-  authors: {
-    type: [mongoose.Schema.Types.ObjectId],
-    required: true,
-    ref: 'Author'
+      required: true,
+    },
+    dimensions: {
+      width: Number,
+      height: Number,
+      depth: Number,
+      unit: {
+        type: String,
+        default: 'cm',
+      },
+    },
+    weight: {
+      value: Number,
+      unit: {
+        type: String,
+        default: 'g',
+      },
+    },
+    authors: {
+      type: [mongoose.Schema.Types.ObjectId],
+      required: true,
+      ref: 'Author',
+    },
+    categories: {
+      type: [String],
+      required: true,
+      index: true,
+    },
   },
-  categories: {
-    type: [String],
-    required: true,
-    index: true
-  }
+  { timestamps: true }
+);
 
-}, { timestamps: true });
+//Add DTOS like spring to validate data and make the data consistence
+BookModel.methods.toEssentials = function () {
+  return {
+    _id: this._id,
+    title: this.title,
+    price: this.price,
+    rating: this.rating,
+    coverImage: this.coverImage,
+    authors: this.authors,
+    categories: this.categories,
+  };
+};
+
+BookModel.methods.toDetails = function () {
+  return {
+    _id: this._id,
+    title: this.title,
+    isbn13: this.isbn13,
+    description: this.description,
+    price: this.price,
+    rating: this.rating,
+    publish_date: this.publish_date,
+    stock: this.stock,
+    coverImage: this.coverImage,
+    dimensions: this.dimensions,
+    weight: this.weight,
+    authors: this.authors,
+    categories: this.categories,
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt,
+  };
+};
+
+//Use methods to use it in queries
+BookModel.statics.findEssentials = function () {
+  return this.find({}, 'title price rating coverImage authors categories');
+};
 
 //check for the id of the authors before save the book to the db
 
