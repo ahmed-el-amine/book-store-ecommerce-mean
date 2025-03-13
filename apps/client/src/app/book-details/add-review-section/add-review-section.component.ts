8aafa3968ccd8fb07872d95678fd66c2a1ce0995
NadaEmamm <nadaemam.me@gmail.com>
2025-03-13 22:04:59 +0200

    book-details -component update the data showed to un signed users

--- apps/client/src/app/book-details/add-review-section/add-review-section.component.ts
+++ apps/client/src/app/book-details/add-review-section/add-review-section.component.ts
@@ -2,8 +2,14 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ReviewService } from '../../service/reviews/review.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';  
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Review } from '../review-section/review-interface';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-add-review-section',
@@ -17,16 +23,36 @@ export class AddReviewSectionComponent {
   submittedSuccessfully = false;
   errorOccurred = false;
   reviews: Review[] = [];
+  userData: any;
+  isAuthenticated = false;
 
-  constructor(private reviewService: ReviewService
+  constructor(private reviewService: ReviewService, private authService: AuthService
   ) {
     this.commentForm = new FormGroup({
-      comment: new FormControl('', [Validators.required, Validators.minLength(30), Validators.maxLength(500)]), 
+      comment: new FormControl('', [Validators.required, Validators.minLength(30), Validators.maxLength(500)]),
       rating: new FormControl('', Validators.required)
     });
   }
 
+
+  ngOnInit(): void {
+    this.authService.isAuthenticated$.subscribe((isAuth) => {
+      this.isAuthenticated = isAuth;
+      if (this.isAuthenticated) {
+        this.authService.currentUser$.subscribe({
+          next: (user) => {
+            console.log('User Data:', user);
+            this.userData = user;
+          },
+          error: (err) => {
+            console.error('Error fetching user data', err);
+          }
+        });
+      }
+    });
+  }
   onSubmit() {
+
     if (this.commentForm.valid && this.bookId) {
       const comment = this.commentForm.value.comment;
       const rating = Number(this.commentForm.value.rating);
@@ -38,7 +64,7 @@ export class AddReviewSectionComponent {
           console.log('done', response);
           this.submittedSuccessfully = true;
           this.errorOccurred = false;
-          this.commentForm.reset(); 
+          this.commentForm.reset();
         },
         error: (err) => {
           console.error('something wrong happend', err);
--- apps/server/src/routes/review.router.js
+++ apps/server/src/routes/review.router.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import useZod from '../middleware/useZod.js';
-import { reviewSchema,patchReviewSchema } from '../lib/zod/review.zod.js';
+import { reviewSchema, patchReviewSchema } from '../lib/zod/review.zod.js';
 import { getByBookId, create, deleteReview, updateReview } from '../controllers/review.controller.js';
 import useAuth from '../middleware/useAuth.middleware.js';
 import { userRoles } from '../database/models/user.model.js';
@@ -8,14 +8,14 @@ import { userRoles } from '../database/models/user.model.js';
 
 const router = express.Router();
 
-router.get('/:id', useAuth([userRoles.admin, userRoles.user]),async (req, res) => {
+router.get('/:id', useAuth([userRoles.admin, userRoles.user]), async (req, res) => {
     const reviews = await getByBookId(req.params.id);
     res.json({ reviewList: reviews }, 200)
 
 })
 
-router.post('/', useAuth([userRoles.admin, userRoles.user]), useZod(reviewSchema),async (req, res) => {
-    const review = await create(req,res);
+router.post('/', useAuth([userRoles.admin, userRoles.user]), useZod(reviewSchema), async (req, res) => {
+    const review = await create(req, res);
     res.json({ message: 'Review created successfully', review }, 201);
 })
 
@@ -24,14 +24,14 @@ router.patch('/:id', useAuth([userRoles.admin, userRoles.user]), useZod(patchRev
     res.json(updatedReview);
 })
 
-router.delete('/:id', useAuth([userRoles.admin, userRoles.user]),async (req, res, next) => {
+router.delete('/:id', useAuth([userRoles.admin, userRoles.user]), async (req, res, next) => {
     const deletedReview = await deleteReview(req, res, next);
     return res.status(200).json({ message: 'Review deleted successfully', deletedReview });
 })
 
 router.get('/', useAuth([userRoles.admin, userRoles.user]), async (req, res) => {
     const id = req.user.id;
-    res.send({id})
+    res.send({ id })
 })
 
 export default router;
